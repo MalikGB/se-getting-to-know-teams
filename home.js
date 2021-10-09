87915edd5d58daccbe02150d429d06e20e802a5c
Jahi Stewart <jahikstewart@lewisu.edu>
2021-10-09 11:07:12 -0500

    Added imagemap

---
+++ home.js
@@ -0,0 +1,30 @@
+const imgmap1= [25,18,45,80];/*Percentages*/
+const imgmap2= [45,18,62,80];
+const imgmap3= [65,18,85,80];
+const link1= "";
+const link2= "";
+const link3= "";
+window.onresize = resizeImageMap;
+var img = new Image();
+img.onload = function(){
+  resizeImageMap()
+}
+img.src = "images/Team-Pic.jpg";
+function resizeImageMap(){
+    let height = document.getElementById('teammap').clientWidth;
+    let width = document.getElementById('teammap').clientWidth;
+    let dimensions = [width, height, width, height];
+    let new_imgmap1=imgmap1.map(function(coord, i ) {
+        return coord/100*dimensions[i];
+      });
+    let new_imgmap2=imgmap2.map(function(coord, i ) {
+        return coord/100*dimensions[i];
+      });
+    let new_imgmap3=imgmap3.map(function(coord, i ) {
+        return coord/100*dimensions[i];
+      });
+    console.log(new_imgmap1,new_imgmap2,new_imgmap3);
+    document.getElementById("resized").innerHTML ='<area shape="rect" coords='+new_imgmap1+' alt="Computer" href='+link1+'>' +
+                                                  '<area shape="rect" coords='+new_imgmap2+' alt="Computer" href='+link2+'>' +     
+                                                  '<area shape="rect" coords='+new_imgmap3+' alt="Computer" href='+link3+'>';
+}
